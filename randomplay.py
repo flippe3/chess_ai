11bbf5704ce50920f5f25ae39dc8f38188211103
Filip Nilsson <filip.c.nilsson@gmail.com>
2022-07-09 19:27:09 +0200

    random play works

--- randomplay.py
+++ randomplay.py
@@ -9,13 +9,11 @@ class RandomAgent:
 
   def make_move(self, state) -> str:
     board = chess.Board(state)
-    print(board)
-    if board.is_checkmate() == False and board.is_insufficient_material() == False:
+    if board.is_game_over() == False: 
       legal_moves = board.legal_moves.count() 
       if legal_moves > 0:
         val = random.randrange(0,legal_moves)
         move = list(board.legal_moves)[val]
-        print(move)
         board.push_uci(str(move))
     return board.fen()
 
